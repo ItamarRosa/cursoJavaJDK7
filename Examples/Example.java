88f9aa15a33307829bb02b3aa8678e6a6c7dacb0
Itamar <itamar.santos@calindra.com.br>
2020-06-29 16:08:18 -0300

    Criando diretório Examples para fins de organização.

---
+++ Examples/Example.java
@@ -0,0 +1,6 @@
+class Example{
+
+	public static void main(String[] args) {
+		System.out.print("Meu primeiro Exemplo Em Java.");
+	}
+}
---
+++ Examples/Example2.java
@@ -0,0 +1,20 @@
+/*
+	Mais um pequeno exemplo
+	Chame este arquivo de Example2.java
+*/
+
+class Example2{
+	public static void main(String args[]){
+		int num; //declara uma variável chamada num
+		
+		num = 100; //atribui a num o valor 100
+
+		System.out.println("Este é num: "+ num);
+
+		num = num * 2;
+
+		System.out.println("O valor de num * 2 é: ");
+
+		System.out.println(num);
+	}
+}
