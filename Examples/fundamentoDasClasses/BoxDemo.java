dbb93715ff9656f7fd0d85e75007bfec43eba9a5
Itamar <itamar.santos@calindra.com.br>
2020-07-09 13:21:44 -0300

    Exemplo simples de classe.

--- Examples/fundamentoDasClasses/BoxDemo.java
+++ Examples/fundamentoDasClasses/BoxDemo.java
@@ -5,6 +5,25 @@ Um programa que usa a classe Box.
 */
 class Box {
     double width;
-    double hight;
+    double height;
     double depth;
+}
+
+//Esta classe declara um objeto do tipo Box.
+class BoxDemo {
+    public static void main(final String[] args) {
+        Box myBox = new Box();
+
+        double vol;
+
+        //atribuindo valores à variśveis de instância de mayBox.
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        // calcular o volume da caixa.
+        vol = myBox.width * myBox.height * myBox.depth;
+
+        System.out.println("O volume é: " + vol);
+    }
 }
